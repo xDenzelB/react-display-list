891f2dc27a63d47c759dec16012815e09c5e3975
xDenzelB <91438105+xDenzelB@users.noreply.github.com>
2022-01-27 10:57:46 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+// import your arrays here
+
+function App() {
+  return (
+    <div className="App">
+        Render all your lists here. Pass the arrays as props.
+    </div>
+  );
+}
+
+export default App;
